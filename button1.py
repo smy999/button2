34756b07b6745a77d3050d124c70289c0df60e41
minyoung <tjalsdud9@naver.com>
2017-11-15 14:22:18 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,12 @@ window =Tk()
 
 b1 = Button(window, text="Minyoung Seo")
 b2 = Button(window, text="60162136")
+b3 = Button(window, text="0113")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
+
+s
